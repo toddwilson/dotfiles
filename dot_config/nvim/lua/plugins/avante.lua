0dcedad14d3a95cfa07b9d3e09024732a2313d3c
Todd Wilson <todd@toddwilson.net>
2025-01-17 10:16:19 -0500

    lazygit, add support for non-sudo setups on linux

--- dot_config/nvim/lua/plugins/avante.lua
+++ dot_config/nvim/lua/plugins/avante.lua
@@ -1,11 +1,10 @@
 return {
   "yetone/avante.nvim",
-  enabled = false,
   event = "VeryLazy",
   lazy = false,
-  version = false, -- set this if you want to always pull the latest change
+  version = false, -- set this to "*" if you want to always pull the latest change, false to update on release
   opts = {
-    provider = "copilot",
+    -- add any opts here
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
   build = "make",
@@ -15,6 +14,7 @@ return {
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
     --- The below dependencies are optional,
+    "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
     "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
     "zbirenbaum/copilot.lua", -- for providers='copilot'
     {
---
+++ dot_config/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,24 @@
+return {
+  "ibhagwan/fzf-lua",
+  opts = function(_, opts)
+    local fzf = require("fzf-lua")
+    local actions = fzf.actions
+
+    -- I use alt-h for aerospace focus, so needed to overwrite hidden toggle here
+    return {
+      files = {
+        cwd_prompt = false,
+        actions = {
+          ["alt-i"] = { actions.toggle_ignore },
+          ["alt-u"] = { actions.toggle_hidden },
+        },
+      },
+      grep = {
+        actions = {
+          ["alt-i"] = { actions.toggle_ignore },
+          ["alt-u"] = { actions.toggle_hidden },
+        },
+      },
+    }
+  end,
+}
